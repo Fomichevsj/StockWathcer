ddd69109c6dd114faaf1b01597737c9f6c288294
Fomichev Sergey <fomichevsj@gmail.com>
2018-01-20 23:56:35 +0300

    initial commit for StockWatcher Copy HTML for app

--- src/com/stockWathcer/client/stockWathcer.java
+++ src/com/stockWathcer/client/stockWathcer.java
@@ -28,14 +28,6 @@ public class stockWathcer implements EntryPoint {
                 }
             }
         });
-
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
     }
 
     private static class MyAsyncCallback implements AsyncCallback<String> {
